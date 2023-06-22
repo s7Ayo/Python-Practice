c4669a92c39fc03d21a7b835e3575f3476130b79
s7Ayo <96587946+s7Ayo@users.noreply.github.com>
2023-06-22 10:12:52 +0100

    first commit

---
+++ romannum.py
@@ -0,0 +1,12 @@
+def solution(roman):
+    roman_numerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    return 1
