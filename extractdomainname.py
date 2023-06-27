d8eb53a387f29cd1f4f3e791d5a5d2896e9dcec3
s7Ayo <96587946+s7Ayo@users.noreply.github.com>
2023-06-27 16:03:10 +0100

    Update extractdomainname.py

--- extractdomainname.py
+++ extractdomainname.py
@@ -1,5 +1,13 @@
+import re
+
+
 def domain_name(url):
-    urllist = [*url]
-    print(urllist)
+    pattern = re.compile(r'(?:https?://)?(?:www\.)?([a-z0-9-]+)\.[a-z]{2,}(?:/[a-z0-9-]+)*', re.IGNORECASE)
+    match = re.search(pattern, url)
+    if match:
+        return match.group(1)  # return the first capturing group
+
+    print(domain_name("https://google.com"))
+    
     pass
-domain_name("httpsdc://googdle.comox")
+domain_name("https://google.com")
